863f6639bdf5a78064e47056fe6c1b3da277732b
Asif Jutt <2019cs219@gmail.com>
2023-12-07 13:20:02 +0500

    Simple To-List in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'TODO List with Angular!';
+  list: any = [];
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+    console.warn(this.list);
+  }
+  removeButton(id: number) {
+    this.list = this.list.filter((item: { id: number }) => item.id !== id);
+  }
 }
